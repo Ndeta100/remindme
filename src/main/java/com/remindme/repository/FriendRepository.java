53f53a2b5ffb7450d26a94b107788b9ffcf8b528
ndeta <ndetainnocent3@gmail.com>
2023-04-11 00:38:38 +0300

    Adding repository interface custom methods

--- src/main/java/com/remindme/repository/FriendRepository.java
+++ src/main/java/com/remindme/repository/FriendRepository.java
@@ -3,5 +3,9 @@ package com.remindme.repository;
 import com.remindme.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FriendRepository extends JpaRepository<Friend,Long> {
+    Optional<Friend> findFriendByUserAccountIdAndPhoneNumber(Long userAccountId, String
+                                                              friendPhoneNumber);
 }
--- src/main/java/com/remindme/repository/OccasionRepository.java
+++ src/main/java/com/remindme/repository/OccasionRepository.java
@@ -4,6 +4,10 @@ import com.remindme.entity.Occasion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OccasionRepository extends JpaRepository<Occasion, Long> {
+    List<Occasion> findOccasionsByDate(int month, int day);
+    Occasion findBirthdayByFriend(Long friendId);
 }
