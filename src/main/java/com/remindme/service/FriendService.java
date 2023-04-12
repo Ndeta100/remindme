e9594bd9ce7d2a967799de4e332ba7048cfcc034
ndeta <ndetainnocent3@gmail.com>
2023-04-12 11:24:49 +0300

    FriendService implementations and methods override

---
+++ src/main/java/com/remindme/service/FriendService.java
@@ -0,0 +1,28 @@
+package com.remindme.service;
+
+import com.remindme.entity.Friend;
+import com.remindme.entity.UserAccount;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface FriendService {
+    public void addNewFriend(UserAccount userAccount, Friend friend);
+    public void deleteFriendById(Long friendId);
+    public void updateFriend(String newFriendPhoneNumber,
+                             String newFriendFirstName,
+                             String newFriendLastName,
+                             String newFriendDOB,
+                             Long friendId);
+    @Transactional
+    public void updateFriendPhoneNumber(String newPhoneNumber, Long friendId);
+    @Transactional
+    public void updateFriendFirstName(String newFirstName, Long friendId);
+    @Transactional
+    public void updateFriendLastName(String newLastName, Long friendId);
+    @Transactional
+    public void updateFriendDOB(LocalDate newFriendDOB, Long friendId);
+    public Friend getFriendById(Long friendId);
+    public List<Friend> getFriendsByUser(String userName);
+}
