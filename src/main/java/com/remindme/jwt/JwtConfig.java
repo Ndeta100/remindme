2d5831b2b00759cf77497582d4295c31bb33b33e
ndeta <ndetainnocent3@gmail.com>
2023-04-17 16:44:30 +0300

    Implementing authentication and authorization

---
+++ src/main/java/com/remindme/jwt/JwtConfig.java
@@ -0,0 +1,28 @@
+package com.remindme.jwt;
+
+import com.google.common.net.HttpHeaders;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix ="remindme")
+@Data
+@NoArgsConstructor
+public class JwtConfig {
+    private String secretKey;
+    private String tokenPrefix;
+    private Integer tokenExpirationAfterDays;
+    public Integer getTokenExpirationAfterDays() {
+        return tokenExpirationAfterDays;
+    }
+
+    public void setTokenExpirationAfterDays(Integer tokenExpirationAfterDays) {
+        this.tokenExpirationAfterDays = tokenExpirationAfterDays;
+    }
+
+    public String getAuthorizationHeader() {
+        return HttpHeaders.AUTHORIZATION;
+    }
+}
