f9ffd71f6a5e5fca34f12b413431b5d5c124e285
ndeta <ndetainnocent3@gmail.com>
2023-04-17 17:11:04 +0300

    Verifying jwt token

---
+++ src/main/java/com/remindme/security/ApplicationSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.remindme.security;
+
+import com.remindme.service.UserAccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.crypto.SecretKey;
+@AllArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfiguration {
+    private final PasswordEncoder passwordEncoder;
+    private final UserAccountService userAccountService;
+    private final SecretKey secretKey;
+
+}
---
+++ src/main/java/com/remindme/security/ApplicationUserPermission.java
@@ -0,0 +1,13 @@
+package com.remindme.security;
+
+public enum ApplicationUserPermission {
+    USER_READ("user:read"),
+    USER_WRITE("user:write");
+        private final String permission;
+    ApplicationUserPermission(String permission) {
+        this.permission=permission;
+    }
+    public String getPermission() {
+        return  permission;
+    }
+}
